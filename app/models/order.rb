5804a46945f745ae0ea82c0951064916edf0c012
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-05-13 09:17:48 -0400

    Move logic to model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,25 @@
 class Order < ApplicationRecord
+  def products
+    product_ids = OrderProduct.where(order_id: params[:order_id]).pluck(:product_id)
+    @products = Product.find(product_ids)
+
+    return @products
+  end
+
+  def shippable?
+    if params[:status] != "shipped" && products?
+      shippable = true
+    else
+      shippable = false
+    end
+  end
+
+  def ship
+    if shippable?
+      @order.update(status: "shipped")
+      return true
+    else
+      return false
+    end
+  end
 end
